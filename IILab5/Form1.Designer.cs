0334d3764e9ed96515f3bc27abc67fb323cafd86
KqkaricK <andrew73-42@yandex.ru>
2021-10-13 22:18:50 +0700

    Добавьте файлы проекта.

---
+++ IILab5/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace IILab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.darkButton1 = new DarkUI.Controls.DarkButton();
+            this.darkTextBox1 = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+            this.t_e = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel2 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel3 = new DarkUI.Controls.DarkLabel();
+            this.darkRadioButton1 = new DarkUI.Controls.DarkRadioButton();
+            this.darkRadioButton2 = new DarkUI.Controls.DarkRadioButton();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // darkButton1
+            // 
+            this.darkButton1.Location = new System.Drawing.Point(15, 84);
+            this.darkButton1.Name = "darkButton1";
+            this.darkButton1.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton1.Size = new System.Drawing.Size(100, 23);
+            this.darkButton1.TabIndex = 0;
+            this.darkButton1.Text = "Пуск";
+            this.darkButton1.Click += new System.EventHandler(this.darkButton1_Click);
+            // 
+            // darkTextBox1
+            // 
+            this.darkTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.darkTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.darkTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkTextBox1.Location = new System.Drawing.Point(15, 12);
+            this.darkTextBox1.Name = "darkTextBox1";
+            this.darkTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.darkTextBox1.TabIndex = 1;
+            this.darkTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.darkTextBox1_KeyPress);
+            // 
+            // darkLabel1
+            // 
+            this.darkLabel1.AutoSize = true;
+            this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel1.Location = new System.Drawing.Point(174, 38);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(10, 13);
+            this.darkLabel1.TabIndex = 2;
+            this.darkLabel1.Text = "-";
+            // 
+            // t_e
+            // 
+            this.t_e.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.t_e.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t_e.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_e.Location = new System.Drawing.Point(215, 12);
+            this.t_e.Name = "t_e";
+            this.t_e.Size = new System.Drawing.Size(100, 20);
+            this.t_e.TabIndex = 3;
+            this.t_e.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.darkTextBox1_KeyPress);
+            // 
+            // darkLabel2
+            // 
+            this.darkLabel2.AutoSize = true;
+            this.darkLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel2.Location = new System.Drawing.Point(174, 61);
+            this.darkLabel2.Name = "darkLabel2";
+            this.darkLabel2.Size = new System.Drawing.Size(10, 13);
+            this.darkLabel2.TabIndex = 4;
+            this.darkLabel2.Text = "-";
+            // 
+            // darkLabel3
+            // 
+            this.darkLabel3.AutoSize = true;
+            this.darkLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel3.Location = new System.Drawing.Point(174, 14);
+            this.darkLabel3.Name = "darkLabel3";
+            this.darkLabel3.Size = new System.Drawing.Size(35, 13);
+            this.darkLabel3.TabIndex = 5;
+            this.darkLabel3.Text = "E = 0,";
+            // 
+            // darkRadioButton1
+            // 
+            this.darkRadioButton1.Checked = true;
+            this.darkRadioButton1.Location = new System.Drawing.Point(15, 38);
+            this.darkRadioButton1.Name = "darkRadioButton1";
+            this.darkRadioButton1.Size = new System.Drawing.Size(110, 17);
+            this.darkRadioButton1.TabIndex = 6;
+            this.darkRadioButton1.TabStop = true;
+            this.darkRadioButton1.Text = "sin (x) + sin (x^2)";
+            // 
+            // darkRadioButton2
+            // 
+            this.darkRadioButton2.AutoSize = true;
+            this.darkRadioButton2.Location = new System.Drawing.Point(15, 61);
+            this.darkRadioButton2.Name = "darkRadioButton2";
+            this.darkRadioButton2.Size = new System.Drawing.Size(123, 17);
+            this.darkRadioButton2.TabIndex = 7;
+            this.darkRadioButton2.Text = "sin (x) + cos (x^3)/3x";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(15, 118);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(828, 216);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 346);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.darkRadioButton2);
+            this.Controls.Add(this.darkRadioButton1);
+            this.Controls.Add(this.darkLabel3);
+            this.Controls.Add(this.darkLabel2);
+            this.Controls.Add(this.t_e);
+            this.Controls.Add(this.darkLabel1);
+            this.Controls.Add(this.darkTextBox1);
+            this.Controls.Add(this.darkButton1);
+            this.Name = "Form1";
+            this.Text = "iIILab5";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DarkUI.Controls.DarkButton darkButton1;
+        private DarkUI.Controls.DarkTextBox darkTextBox1;
+        private DarkUI.Controls.DarkLabel darkLabel1;
+        private DarkUI.Controls.DarkTextBox t_e;
+        private DarkUI.Controls.DarkLabel darkLabel2;
+        private DarkUI.Controls.DarkLabel darkLabel3;
+        private DarkUI.Controls.DarkRadioButton darkRadioButton1;
+        private DarkUI.Controls.DarkRadioButton darkRadioButton2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ IILab5/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarkUI.Forms;
+
+namespace IILab5
+{
+    public partial class Form1 : DarkForm
+    {
+        const double d = 0.99;
+        const int MAX_POSIBALITY = 1000;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double Proiz1(double x)
+        {
+            return Math.Cos(x) + 2 * x * Math.Cos(x * x);
+        }
+        double Norm(double x, double y)
+        {
+            return Math.Sqrt((x * x) + (y * y));
+        }
+        double Proiz2(double x)
+        {
+            return Math.Cos(x) + x * Math.Sin(x * x * x);
+        }
+
+        void Action1()
+        {
+            double E = Convert.ToDouble("0," + t_e.Text);
+            double x0 = Convert.ToDouble(darkTextBox1.Text);
+            double x1 = x0 - d * Proiz1(x0) / Norm(x0, Proiz1(x0));
+            int i = 1;
+            chart1.Series[0].Points.AddXY(i, x1);
+            while (Math.Abs(x1 - x0) > E)
+            {
+                if (i == MAX_POSIBALITY)
+                    break;
+                x0 = x1;
+                x1 = x0 - d * Proiz1(x0) / Norm(x0, Proiz1(x0));
+                i++;
+                chart1.Series[0].Points.AddXY(i, x1);
+            }
+            darkLabel1.Text = x1.ToString();
+            darkLabel2.Text = i.ToString();
+        }
+        void Action2()
+        {
+            double E = Convert.ToDouble("0," + t_e.Text);
+            double x0 = Convert.ToDouble(darkTextBox1.Text);
+            double x1 = x0 - d * Proiz2(x0) / Norm(x0, Proiz2(x0));
+            int i = 1;
+            chart1.Series[0].Points.AddXY(i, x1);
+            while (Math.Abs(x1 - x0) > E)
+            {
+                if (i == MAX_POSIBALITY)
+                    break;
+                x0 = x1;
+                x1 = x0 - d * Proiz2(x0) / Norm(x0, Proiz2(x0));
+                i++;
+                chart1.Series[0].Points.AddXY(i, x1);
+            }
+            darkLabel1.Text = x1.ToString();
+            darkLabel2.Text = i.ToString();
+        }
+
+        private void darkButton1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            if (darkRadioButton1.Checked == true)
+            {
+                Action1();
+            }
+            else
+            {
+                Action2();
+            }
+        }
+        private void darkTextBox1_KeyPress(object sender, KeyPressEventArgs e) //запрет на ввод
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 45 && number != 44)
+            {
+                e.Handled = true;
+            }
+        }
+        private void darkTextBox2_KeyPress(object sender, KeyPressEventArgs e) //запрет на ввод
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
